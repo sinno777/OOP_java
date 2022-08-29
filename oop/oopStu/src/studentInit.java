2ad4c8db2261731423b0f720a1eb623dc9828c82
KhoaSN <khoahotboy12345@gmail.com>
2022-08-29 09:49:11 +0700

    ajaj

---
+++ oop/oopStu/src/studentInit.java
@@ -0,0 +1,13 @@
+public class studentInit {
+    public String name;
+    public String email;
+
+    public studentInit(){
+
+    }
+
+    public studentInit(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+}
